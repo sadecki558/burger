7e1961babecc63445d94c3590faaf8ed6e9b5125
sadecki558 <matthewsadecki@gmail.com>
2018-09-06 13:26:41 -0400

    added gitignore

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
-INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
+INSERT INTO burgers (burger_name) VALUES ('Get Piggy With It Burger');
