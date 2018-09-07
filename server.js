3506e2ef013d69f51ecdb67ea78018be62ca07f8
sadecki558 <matthewsadecki@gmail.com>
2018-09-06 20:43:57 -0400

    possible fix

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgers_controller.js");
 const app = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
